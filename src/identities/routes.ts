83c1c911042c65047d014d9124b59cd8e63e5943
Henrik Hofmeister <henrik@newdawn.dk>
2023-07-22 21:58:08 +0200

    feat: Added route for uploading attachments to assets

--- src/identities/routes.ts
+++ src/identities/routes.ts
@@ -10,11 +10,20 @@ const api = new KapetaAPI();
 router.use('/', corsHandler);
 
 router.get('/current', async (req: Request, res: Response) => {
-    res.send(await api.getCurrentIdentity());
+    try {
+        res.send(await api.getCurrentIdentity());
+    } catch (e:any) {
+        res.status(e.status ?? 500).send(e);
+    }
+
 });
 
 router.get('/:identityId/memberships', async (req: Request, res: Response) => {
-    res.send(await api.getMemberships(req.params.identityId));
+    try {
+        res.send(await api.getMemberships(req.params.identityId));
+    } catch (e:any) {
+        res.status(e.status ?? 500).send(e);
+    }
 });
 
 export default router;
